b426a1cd8a32b9a11065db96867acc8c95087abf
Nico Picchio <nicolapicchio@gmail.com>
2022-03-23 15:56:04 +0000

    standard criteria part 1 (films) completed

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = 3030;
 
-const cors = require("cors");
-const morgan = require("morgan");
+const cors = require('cors');
+const morgan = require('morgan');
 
-const data = require('./data.js')
+const data = require('./data.js');
 
 // SETUP MIDDLEWARE
 app.use(cors());
 app.use(express.json());
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 
 // REQUIRE ROUTERS
-const usersRouter = require("./src/routers/users");
-app.use('/users', usersRouter)
+const usersRouter = require('./src/routers/users');
+const filmsRouter = require('./src/routers/films');
+// const booksRouter = require('./src/routers/books');
+app.use('/users', usersRouter);
+app.use('/films', filmsRouter);
+// app.use('/books', booksRouter);
 
 /* START SERVER */
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}/`);
+	console.log(`Server is running on http://localhost:${port}/`);
 });
